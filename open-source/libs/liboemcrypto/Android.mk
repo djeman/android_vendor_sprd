5543e759ad5fc0f1302c4b120916033fd8fd186a
djeman <djeman@home.fr>
2017-07-19 01:54:40 +0200

    changes liboemcrypto static to shared

--- open-source/libs/liboemcrypto/Android.mk
+++ open-source/libs/liboemcrypto/Android.mk
@@ -6,17 +6,18 @@ LOCAL_PRELINK_MODULE := false
 LOCAL_C_INCLUDES += \
 		$(LOCAL_PATH)/include \
                 $(TOP)/external/boringssl/src/include
-              
+
+LOCAL_C_INCLUDES += $(common_C_INCLUDES)
 
 LOCAL_SRC_FILES:= \
 		  src/sprdoemcrypto.c
 
 LOCAL_MODULE := liboemcrypto
-
-LOCAL_C_INCLUDES += $(common_C_INCLUDES)
-
 LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_OWNER := sprd
+LOCAL_PROPRIETARY_MODULE := true
 
-LOCAL_SHARED_LIBRARIES:=liblog libcutils libc
+LOCAL_SHARED_LIBRARIES := libcrypto liblog libcutils libc
 
-include $(BUILD_STATIC_LIBRARY)
+include $(BUILD_SHARED_LIBRARY)
