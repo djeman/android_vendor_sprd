aca0bd0cfb1d01d126939326f4ee97eb2a121e15
djeman <djeman@home.fr>
2017-08-12 18:25:47 +0200

    move sprd proprietaries to vendor/sprd

---
+++ proprietaries/proprietaries-scx35l.mk
@@ -0,0 +1,24 @@
+LOCAL_PATH := vendor/sprd/proprietaries
+
+# Proprietaries drm
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/scx35l/system/etc/permissions/com.google.widevine.software.drm.xml:system/etc/permissions/com.google.widevine.software.drm.xml \
+    $(LOCAL_PATH)/scx35l/system/framework/com.google.widevine.software.drm.jar:system/framework/com.google.widevine.software.drm.jar \
+    $(LOCAL_PATH)/scx35l/system/lib/libdrmdecrypt.so:system/lib/libdrmdecrypt.so \
+    $(LOCAL_PATH)/scx35l/system/vendor/lib/drm/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so \
+    $(LOCAL_PATH)/scx35l/system/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so \
+    $(LOCAL_PATH)/scx35l/system/vendor/lib/libwvdrm_L3.so:system/vendor/lib/libwvdrm_L3.so \
+    $(LOCAL_PATH)/scx35l/system/vendor/lib/libwvm.so:system/vendor/lib/libwvm.so \
+    $(LOCAL_PATH)/scx35l/system/vendor/lib/libWVStreamControlAPI_L3.so:system/vendor/lib/libWVStreamControlAPI_L3.so
+
+# Proprietaries libomx
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/scx35l/system/lib/libomx_avcdec_hw_sprd.so:system/lib/libomx_avcdec_hw_sprd.so \
+    $(LOCAL_PATH)/scx35l/system/lib/libomx_avcdec_sw_sprd.so:system/lib/libomx_avcdec_sw_sprd.so \
+    $(LOCAL_PATH)/scx35l/system/lib/libomx_avcenc_hw_sprd.so:system/lib/libomx_avcenc_hw_sprd.so \
+    $(LOCAL_PATH)/scx35l/system/lib/libomx_m4vh263dec_hw_sprd.so:system/lib/libomx_m4vh263dec_hw_sprd.so \
+    $(LOCAL_PATH)/scx35l/system/lib/libomx_m4vh263dec_sw_sprd.so:system/lib/libomx_m4vh263dec_sw_sprd.so \
+    $(LOCAL_PATH)/scx35l/system/lib/libomx_m4vh263enc_hw_sprd.so:system/lib/libomx_m4vh263enc_hw_sprd.so \
+    $(LOCAL_PATH)/scx35l/system/lib/libomx_mp3dec_sprd.so:system/lib/libomx_mp3dec_sprd.so \
+    $(LOCAL_PATH)/scx35l/system/lib/libomx_vpxdec_hw_sprd.so:system/lib/libomx_vpxdec_hw_sprd.so
+
