79760ea7edd1d0de1c784662f7d979209ad8a2a0
djeman <djemouli@gmail.com>
2017-08-20 15:20:13 +0200

    widevine: upgrade for android 7
    Change-Id: I6394ca0cb3383b80ed802770b820e3d627c12f3e

--- proprietaries/proprietaries-scx35l.mk
+++ proprietaries/proprietaries-scx35l.mk
@@ -4,11 +4,9 @@ LOCAL_PATH := vendor/sprd/proprietaries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/scx35l/system/etc/permissions/com.google.widevine.software.drm.xml:system/etc/permissions/com.google.widevine.software.drm.xml \
     $(LOCAL_PATH)/scx35l/system/framework/com.google.widevine.software.drm.jar:system/framework/com.google.widevine.software.drm.jar \
-    $(LOCAL_PATH)/scx35l/system/lib/libdrmdecrypt.so:system/lib/libdrmdecrypt.so \
-    $(LOCAL_PATH)/scx35l/system/vendor/lib/drm/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so \
     $(LOCAL_PATH)/scx35l/system/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so \
+    $(LOCAL_PATH)/scx35l/system/vendor/lib/mediadrm/libdrmclearkeyplugin.so:system/vendor/lib/mediadrm/libdrmclearkeyplugin.so \
     $(LOCAL_PATH)/scx35l/system/vendor/lib/libwvdrm_L3.so:system/vendor/lib/libwvdrm_L3.so \
-    $(LOCAL_PATH)/scx35l/system/vendor/lib/libwvm.so:system/vendor/lib/libwvm.so \
     $(LOCAL_PATH)/scx35l/system/vendor/lib/libWVStreamControlAPI_L3.so:system/vendor/lib/libWVStreamControlAPI_L3.so
 
 # Proprietaries libomx
