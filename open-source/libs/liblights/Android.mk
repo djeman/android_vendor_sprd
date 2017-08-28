353a380c02d9901055a44fe80c3b032b2de96b8d
djeman <djemouli@gmail.com>
2017-08-28 16:39:09 +0200

    add missing dependancy after BOARD_VENDOR flag removed
    Change-Id: I07016ad714b9b90636ba07a89c01bb4c7289f73b

--- open-source/libs/liblights/Android.mk
+++ open-source/libs/liblights/Android.mk
@@ -16,6 +16,21 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+LOCAL_SRC_FILES := lights_helper.c
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/include
+
+LOCAL_SHARED_LIBRARIES := liblog
+
+LOCAL_MODULE := liblights_helper
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_STATIC_LIBRARY)
+
+
+include $(CLEAR_VARS)
+
 LOCAL_SRC_FILES := lights.c
 
 LOCAL_SHARED_LIBRARIES := liblog
