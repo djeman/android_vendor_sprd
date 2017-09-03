54cd5939c81e8dc69b95e1447263eaf1821b8825
djeman <djemouli@gmail.com>
2017-09-03 18:16:55 +0200

    widevine: remove libdrmclearkeyplugin
    Change-Id: I43bfe24717dbe0c4cbbd180bb07ffa24d509a95c

--- proprietaries/proprietaries-scx35l.mk
+++ proprietaries/proprietaries-scx35l.mk
@@ -5,7 +5,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/scx35l/system/etc/permissions/com.google.widevine.software.drm.xml:system/etc/permissions/com.google.widevine.software.drm.xml \
     $(LOCAL_PATH)/scx35l/system/framework/com.google.widevine.software.drm.jar:system/framework/com.google.widevine.software.drm.jar \
     $(LOCAL_PATH)/scx35l/system/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so \
-    $(LOCAL_PATH)/scx35l/system/vendor/lib/mediadrm/libdrmclearkeyplugin.so:system/vendor/lib/mediadrm/libdrmclearkeyplugin.so \
     $(LOCAL_PATH)/scx35l/system/vendor/lib/libwvdrm_L3.so:system/vendor/lib/libwvdrm_L3.so \
     $(LOCAL_PATH)/scx35l/system/vendor/lib/libWVStreamControlAPI_L3.so:system/vendor/lib/libWVStreamControlAPI_L3.so
 
