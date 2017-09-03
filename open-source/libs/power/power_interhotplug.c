d4b335bd3f7e95d6fca751ac6dccef467271e4aa
djeman <djemouli@gmail.com>
2017-09-03 04:45:05 +0200

    power: remove GNU old-style field designator extension
    Change-Id: Ia07cb082302ae332c2dfddda2d6f8c244bd66ab7

--- open-source/libs/power/power_interhotplug.c
+++ open-source/libs/power/power_interhotplug.c
@@ -160,24 +160,24 @@ static struct hw_module_methods_t power_module_methods = {
 };
 
 struct interhotplug_power_module HAL_MODULE_INFO_SYM = {
-    base: {
-        common: {
-            tag: HARDWARE_MODULE_TAG,
-            module_api_version: POWER_MODULE_API_VERSION_0_2,
-            hal_api_version: HARDWARE_HAL_API_VERSION,
-            id: POWER_HARDWARE_MODULE_ID,
-            name: "InterHotplug Power HAL",
-            author: "The Android Open Source Project",
-            methods: &power_module_methods,
+    .base = {
+        .common = {
+            .tag = HARDWARE_MODULE_TAG,
+            .module_api_version = POWER_MODULE_API_VERSION_0_2,
+            .hal_api_version = HARDWARE_HAL_API_VERSION,
+            .id = POWER_HARDWARE_MODULE_ID,
+            .name = "InterHotplug Power HAL",
+            .author = "The Android Open Source Project",
+            .methods = &power_module_methods,
         },
 
-        init: power_init,
-        setInteractive: power_set_interactive,
-        powerHint: interhotplug_power_hint,
+        .init = power_init,
+        .setInteractive = power_set_interactive,
+        .powerHint = interhotplug_power_hint,
     },
 
-    lock: PTHREAD_MUTEX_INITIALIZER,
-    boostpulse_fd: -1,
-    boostpulse_warned: 0,
+    .lock = PTHREAD_MUTEX_INITIALIZER,
+    .boostpulse_fd = -1,
+    .boostpulse_warned = 0,
 };
 
