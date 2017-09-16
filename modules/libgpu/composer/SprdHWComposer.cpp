10fab652d99bf534368cc10e145ad658b86c8ead
djeman <djemouli@gmail.com>
2017-09-16 02:09:56 +0200

    hwcomposer: remove old gnu style warnings
    Change-Id: I8ad82c3d293c4623f5fcd745cf7b539b79470dd0

--- modules/libgpu/composer/SprdHWComposer.cpp
+++ modules/libgpu/composer/SprdHWComposer.cpp
@@ -833,19 +833,19 @@ static int hwc_device_open(const struct hw_module_t* module, const char* name, s
 }
 
 static struct hw_module_methods_t hwc_module_methods = {
-    open: hwc_device_open
+    .open = hwc_device_open
 };
 
 hwc_module_t HAL_MODULE_INFO_SYM = {
-    common: {
-        tag: HARDWARE_MODULE_TAG,
-        version_major: 3,
-        version_minor: 0,
-        id: HWC_HARDWARE_MODULE_ID,
-        name: "SPRD HWComposer Module",
-        author: "The AndroidL Open Source Project",
-        methods: &hwc_module_methods,
-        dso: 0,
-        reserved: {0},
+    .common = {
+        .tag = HARDWARE_MODULE_TAG,
+        .version_major = 3,
+        .version_minor = 0,
+        .id = HWC_HARDWARE_MODULE_ID,
+        .name = "SPRD HWComposer Module",
+        .author = "The AndroidL Open Source Project",
+        .methods = &hwc_module_methods,
+        .dso = 0,
+        .reserved = {0},
     }
 };
